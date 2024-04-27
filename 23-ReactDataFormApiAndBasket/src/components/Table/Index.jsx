fbc5df871514e04faafe4e814902a373116eb88d
DinareAydemirova <dinaraaydamirova@gmail.com>
2024-04-27 21:43:18 +0400

    delete

--- 23-ReactDataFormApiAndBasket/src/components/Table/Index.jsx
+++ 23-ReactDataFormApiAndBasket/src/components/Table/Index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import React from "react";
 import { Button, Table } from "react-bootstrap";
 import { v4 as uuidv4 } from "uuid";
+import style from './style.module.css'
 
 const DataTable = () => {
   const [data, setData] = useState([]);
@@ -16,25 +17,29 @@ const DataTable = () => {
   }, []);
 
   return (
-    <Table striped bordered hover>
+    <Table striped bordered hover className={style.table}>
       <thead>
         <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>unitPrice</th>
-          <th>Delete</th>
-          <th>Edit</th>
+          <th  className={style.headings}>ID</th>
+          <th  className={style.headings}>Name</th>
+          <th  className={style.headings}>unitPrice</th>
+          <th  className={style.headings}>Delete</th>
+          <th  className={style.headings}>Edit</th>
         </tr>
       </thead>
       <tbody>
         {data.map((elem) => {
           return (
             <tr key={uuidv4()}>
-              <td>{elem.id}</td>
-              <td>{elem.name}</td>
-              <td>{elem.unitPrice}</td>
-              <td><Button variant="danger">Delete</Button></td>
-              <td><Button variant="primary">Edit</Button></td>
+              <td className={style.datas}>{elem.id}</td>
+              <td className={style.datas}>{elem.name}</td>
+              <td className={style.datas}>{elem.unitPrice}</td>
+              <td className={style.datas} ><Button className={style.del} onClick={()=>{
+                let arr= data.filter(e=>e.id != elem.id)
+                setData(arr)
+                axios.delete(`https://northwind.vercel.app/api/products/${elem.id}`)
+              }}>Delete</Button></td>
+              <td className={style.datas}><Button className={style.edit}>Edit</Button></td>
             </tr>
           );
         })}
