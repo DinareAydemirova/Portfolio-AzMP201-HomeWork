c487f88e811e131717529f811e6badf2f9225ed9
DinareAydemirova <dinaraaydamirova@gmail.com>
2024-05-03 11:53:42 +0400

    style

--- 25-AdminPanelWithReducer/src/Pages/Admin/Index.jsx
+++ 25-AdminPanelWithReducer/src/Pages/Admin/Index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { BASE_URL } from "../../services/api";
+import { BASE_URL, endPoints } from "../../services/api";
 import { Link } from "react-router-dom";
 
 const Admin = ({ state, dispatch }) => {
@@ -264,7 +264,7 @@ const Admin = ({ state, dispatch }) => {
                                   );
                                   setData([...arr]);
                                   axios.delete(
-                                    `https://fakestoreapi.com/products/${elem.id}`
+                                    `${BASE_URL + endPoints.products +elem.id}`
                                   );
                                 }}
                               >
--- 25-AdminPanelWithReducer/src/Pages/Post/Index.jsx
+++ 25-AdminPanelWithReducer/src/Pages/Post/Index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { BASE_URL } from "../../services/api";
+import { BASE_URL, endPoints } from "../../services/api";
 
 const Post = () => {
   const [inputValues, setInputValues] = useState({
@@ -15,7 +15,7 @@ const Post = () => {
     },
   });
   return (
-    <form className="w-9/12 mt-20" >
+    <form className="w-9/12 mt-20">
       <div className="md:flex md:items-center mb-6">
         <div className="md:w-1/3">
           <label
@@ -30,9 +30,9 @@ const Post = () => {
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-full-name"
             value={inputValues.image}
-            type="text"  onChange={(e)=>{
-              setInputValues({...inputValues, image:e.target.value})
-
+            type="text"
+            onChange={(e) => {
+              setInputValues({ ...inputValues, image: e.target.value });
             }}
           />
         </div>
@@ -50,8 +50,10 @@ const Post = () => {
           <input
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-password"
-            type="text" value={inputValues.title} onChange={(e)=>{
-              setInputValues({...inputValues, title:e.target.value})
+            type="text"
+            value={inputValues.title}
+            onChange={(e) => {
+              setInputValues({ ...inputValues, title: e.target.value });
             }}
           />
         </div>
@@ -69,8 +71,10 @@ const Post = () => {
           <input
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-password"
-            type="text" value={inputValues.price} onChange={(e)=>{
-              setInputValues({...inputValues, price:e.target.value})
+            type="text"
+            value={inputValues.price}
+            onChange={(e) => {
+              setInputValues({ ...inputValues, price: e.target.value });
             }}
           />
         </div>
@@ -79,7 +83,7 @@ const Post = () => {
         <div className="md:w-1/3">
           <label
             className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            htmlFor="inline-password" 
+            htmlFor="inline-password"
           >
             Category
           </label>
@@ -88,8 +92,10 @@ const Post = () => {
           <input
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-password"
-            type="text" value={inputValues.category} onChange={(e)=>{
-              setInputValues({...inputValues, category:e.target.value})
+            type="text"
+            value={inputValues.category}
+            onChange={(e) => {
+              setInputValues({ ...inputValues, category: e.target.value });
             }}
           />
         </div>
@@ -107,8 +113,13 @@ const Post = () => {
           <input
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-password"
-            type="text" value={inputValues.rating.rate} onChange={(e)=>{
-              setInputValues({...inputValues, rating:{rate:e.target.value}})
+            type="text"
+            value={inputValues.rating.rate}
+            onChange={(e) => {
+              setInputValues({
+                ...inputValues,
+                rating: { rate: e.target.value },
+              });
             }}
           />
         </div>
@@ -126,8 +137,13 @@ const Post = () => {
           <input
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-password"
-            type="text" value={inputValues.rating.count} onChange={(e)=>{
-              setInputValues({...inputValues, rating:{count:e.target.value}})
+            type="number"
+            value={inputValues.rating.count}
+            onChange={(e) => {
+              setInputValues({
+                ...inputValues,
+                rating: { count: e.target.value },
+              });
             }}
           />
         </div>
@@ -138,9 +154,21 @@ const Post = () => {
         <div className="md:w-2/3">
           <button
             className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            type="submit" onClick={(e)=>{
-              e.preventDefault()
-              axios.post(BASE_URL, inputValues)
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              axios.post(BASE_URL + endPoints.products, inputValues);
+              setInputValues({
+                image: "",
+                title: "",
+                price: "",
+                category: "",
+
+                rating: {
+                  rate: "",
+                  count: "",
+                },
+              });
             }}
           >
             Submit
